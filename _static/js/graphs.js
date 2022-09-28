91ec848913ae9a7f0afdd86cc59801f135dd305f
Dixon Yant <dixon@gatetothewest.com>
2022-09-27 21:14:01 -0500

    Ref: remove old and unused code
    can use this commit as reference

--- _static/js/graphs.js
+++ _static/js/graphs.js
@@ -247,15 +247,6 @@ $(document).ready(function () {
         });
     }
 
-    // data: full import data
-    // ieType: object keys of config, the ie
-    function getLabels(data, ieType) {
-        return data
-            .filter((item) => item[1] === ieType)
-            .map((item) => item[2]);
-    }
-
-
     // this is the graph data that is used to render the graphs
     // needs to be built after the filter is selected, not split by ie-types
     var CONFIG = {
@@ -333,36 +324,6 @@ $(document).ready(function () {
         }
     }
 
-    // doesn't seem to be used anywhere so might remove after first main build
-    var titleMapping = {
-        core: '<h3>Intel® Core™</h3>',
-        atom: '<h3>Intel® Atom®</h3>',
-        xeon: '<h3>Intel® Xeon®</h3>',
-        accel: '<h3>Intel® Movidius™ Vision Processing Units</h3>'
-    }
-
-
-    // since this is using the ie-type split, will need to refactor away from this
-    var labelsMapping = {
-        core: null,
-        atom: null,
-        xeon: null,
-        accel: null
-    }
-
-    /**
-     * params:
-     * data: (all imported data)
-     * labels: 
-     * 
-     *
-     * */
-
-    function getDataByLabelsAndIndex(data, labels, pos) {
-        // need to refactor not by array index but property map
-        return data.filter(item => labels.indexOf(item[2]) !== -1).map(item => parseFloat(item[pos]));
-    }
-
     function getChartOptions(title, displayLabels) {
         return {
             responsive: false,
@@ -402,22 +363,6 @@ $(document).ready(function () {
         }
     }
 
-    function getChartData(hwType, metric) {
-        //console.log(CONFIG.hwType.metric.datasets);
-        return {
-            labels: labelsMapping[hwType],
-            datasets: CONFIG[hwType][metric]['datasets'].map(function (item) {
-                return {
-                    label: item.label,
-                    data: item.data,
-                    backgroundColor: item.color,
-                    borderColor: 'rgba(170,170,170,0)',
-                    barThickness: 12
-                }
-            })
-        };
-    }
-
     // params: string[], Datasets[]
     function getChartDataNew(labels, datasets) {
         return {
@@ -438,8 +383,6 @@ $(document).ready(function () {
         return function (result) {
             console.log(result);
             var data = result.data;
-
-
             var NETWORKMODEL = getNetworkModel();
             var IETYPE = getIeType();
             var KPIS = getKPI();
@@ -556,78 +499,6 @@ $(document).ready(function () {
 
         }
 
-        function createChart(data, hwType, chartContainer) {
-            // add title
-            var chartWrap = $('<div>');
-            chartWrap.addClass('chart-wrap');
-            chartWrap.addClass('container');
-            chartContainer.append(chartWrap);
-            var labels = getLabels(data, hwType);
-            var int8Data = getDataByLabelsAndIndex(data, labels, 3);
-            var fp32Data = getDataByLabelsAndIndex(data, labels, 4);
-            var fp16Data = getDataByLabelsAndIndex(data, labels, 5);
-            var valueData = getDataByLabelsAndIndex(data, labels, 6);
-            var efficiencyData = getDataByLabelsAndIndex(data, labels, 7);
-            var latencyData = getDataByLabelsAndIndex(data, labels, 8);
-
-            labelsMapping[hwType] = labels
-            if (hwType === 'accel') {
-                CONFIG[hwType].throughput.datasets[0].data = fp16Data;
-            }
-            else {
-                CONFIG[hwType].throughput.datasets[0].data = int8Data;
-                CONFIG[hwType].throughput.datasets[1].data = fp32Data;
-            }
-            CONFIG[hwType].latency.datasets[0].data = latencyData;
-            CONFIG[hwType].value.datasets[0].data = valueData;
-            CONFIG[hwType].efficiency.datasets[0].data = efficiencyData;
-
-            metrics = Object.keys(CONFIG[hwType]).filter((metric) => hasData(hwType, metric));
-
-            console.log(CONFIG);
-            var througputLatency = $('<div>');
-            througputLatency.addClass('row');
-            var efficiencyValue = $('<div>');
-            efficiencyValue.addClass('row');
-
-            chartWrap.append(througputLatency);
-            chartWrap.append(efficiencyValue);
-
-            var displayWidth = $(window).width();
-
-            if (metrics.includes('throughput') && metrics.includes('latency')) {
-                processMetric(hwType, 'throughput', througputLatency, 'col-md-8', true);
-                if (displayWidth < 450) {
-                    processMetric(hwType, 'latency', througputLatency, 'col-md-4', true);
-                }
-                else {
-                    processMetric(hwType, 'latency', througputLatency, 'col-md-4', false);
-                }
-            }
-            else if (metrics.includes('throughput')) {
-                processMetric(hwType, 'throughput', througputLatency, 'col-md-12', true);
-            }
-            else if (metrics.includes('latency')) {
-                processMetric(hwType, 'latency', througputLatency, 'col-md-12', true);
-            }
-
-            if (metrics.includes('efficiency') && metrics.includes('value')) {
-                processMetric(hwType, 'efficiency', througputLatency, 'col-md-8', true);
-                if (displayWidth < 450) {
-                    processMetric(hwType, 'value', througputLatency, 'col-md-4', true);
-                }
-                else {
-                    processMetric(hwType, 'value', througputLatency, 'col-md-4', false);
-                }
-            }
-            else if (metrics.includes('efficiency')) {
-                processMetric(hwType, 'efficiency', througputLatency, 'col-md-6', true);
-            }
-            else if (metrics.includes('value')) {
-                processMetric(hwType, 'value', througputLatency, 'col-md-6', true);
-            }
-        }
-
         function processMetricNew(labels, datasets, chartTitle, container, widthClass, displayLabels) {
             var chart = $('<div>');
             chart.addClass('chart');
@@ -649,42 +520,6 @@ $(document).ready(function () {
                 data: getChartDataNew(labels, datasets),
                 options: getChartOptions(chartTitle, displayLabels)
             });
-        }
-
-        function processMetric(hwType, metric, container, widthClass, displayLabels) {
-            var chart = $('<div>');
-            chart.addClass('chart');
-            chart.addClass(widthClass);
-            chart.height(labelsMapping[hwType].length * 55 + 30);
-            var canvas = $('<canvas>');
-            chart.append(canvas);
-            container.append(chart);
-            var context = canvas.get(0).getContext('2d');
-            context.canvas.height = labelsMapping[hwType].length * 55 + 30;
-            if (widthClass === 'col-md-8') {
-                context.canvas.width = context.canvas.width * 1.5;
-            }
-            else if (widthClass === 'col-md-12') {
-                context.canvas.width = context.canvas.width * 2.5;
-            }
-            new Chart(context, {
-                type: 'horizontalBar',
-                data: getChartData(hwType, metric),
-                options: getChartOptions(CONFIG[hwType][metric].chartTitle, displayLabels)
-            });
-        }
-
-        function hasData(hwType, metric) {
-            var has = false;
-            CONFIG[hwType][metric]['datasets'].forEach(function (dataset) {
-                for (var i = 0; i < dataset.data.length; i++) {
-                    if (dataset.data[i] > 0) {
-                        has = true;
-                        break;
-                    }
-                }
-            })
-            return has;
         }
     }
 
